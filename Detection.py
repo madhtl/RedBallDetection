cdf5bcf107110cd8bc04569a62bff3712bbe87ff
madhtl <s28795@pjwstk.edu.pl>
2025-04-08 21:24:23 +0200

    Chore: Video Detection

--- Detection.py
+++ Detection.py
@@ -2,12 +2,11 @@ import cv2
 import numpy as np
 import sys
 
-
-def imshow(title, image, scale=0.2):
+def imshow(title, img, scale=0.2):
     if img is None:
         sys.exit("Could not read the image.")
-    new_size = (int(image.shape[1] * scale), int(image.shape[0] * scale))
-    resized_image = cv2.resize(image, new_size)
+    new_size = (int(img.shape[1] * scale), int(img.shape[0] * scale))
+    resized_image = cv2.resize(img, new_size)
     cv2.imshow(title, resized_image)
     cv2.waitKey(0)  # wait forever
     cv2.destroyAllWindows()
@@ -19,44 +18,54 @@ upper_red1 = np.array([15, 255, 255])
 lower_red2 = np.array([180-15, 50, 20])
 upper_red2 = np.array([180, 255, 255])
 
+def centroid(mask, img):
+    #img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #imshow("The img_gray", img_gray)
+    #thresh = cv2.threshold(img_gray, 20,255 , cv2.THRESH_BINARY)[1]
+    M = cv2.moments(mask)
+    print(M)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    cv2.circle(img, (cX, cY), 400, (255, 0, 0), -1)
+    cv2.putText(img, "Centroid", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0,0,0), 9)
+    imshow("Centroid Detected", img)
 
 def detect_red_ball(img):
-    # Convert image to HSV color space
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # Create masks for two ranges of red
     mask1 = cv2.inRange(img_hsv, lower_red1, upper_red1)
     mask2 = cv2.inRange(img_hsv, lower_red2, upper_red2)
-
-    # Combine both red masks
     mask = cv2.bitwise_or(mask1, mask2)
+    #imshow("Masks combined", mask)
 
-    # remove noise
-    kernel = np.ones((7,7), np.uint8)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    largest_contour = max(contours, key=cv2.contourArea)
+    print(largest_contour)
+
+    kernel = np.ones((4,4), np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    #imshow("Mask after noise removal", mask)
 
-    imshow("Mask after noise removal", mask)
-
-    # Segment the image using the mask
-    seg_img = cv2.bitwise_and(img, img, mask=mask)
-    imshow("Segmented Image", seg_img)
-    return seg_img
-
-#def centroidcalc(img):
+    cv2.drawContours(img, [largest_contour], -1, (255, 0, 0), 5, cv2.LINE_AA)
+    imshow("temp_output", img)
+    return img, mask
+'''
 def binarizeimg(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    binr = cv2.threshold(img_gray, 80, 255, cv2.THRESH_OTSU)[1]
+    binr = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU)[1]
     imshow("Binary_img", binr)
     kernel = np.ones((5, 5), np.uint8)
     opening = cv2.morphologyEx(binr, cv2.MORPH_OPEN, kernel, iterations=1)
     imshow("opening", opening)
     closing = cv2.morphologyEx(binr, cv2.MORPH_CLOSE, kernel, iterations=3)
     imshow("closing", closing)
+    return opening
+'''
+
+#image = cv2.imread("RedBallPhoto.jpeg")
+#processed_img, mask = detect_red_ball(image)
+#centroid(mask, processed_img)
 
 
-# Load the image and detect the red ball
-img = cv2.imread("RedBallPhoto.jpeg")
-#imshow("Original Image", img)
-#detect_red_ball(img)
-binarizeimg(img)
+
---
+++ VideoDetection.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+from Detection import detect_red_ball, centroid
+
+cap = cv.VideoCapture("rgb_ball_720.mp4")
+frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv.CAP_PROP_FPS)
+
+output = cv.VideoWriter("output_with_centroid.mp4", cv.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    processed_img, mask1 = detect_red_ball(frame)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    final_frame = centroid(mask1, processed_img)
+    output.write(final_frame)
+    cv.imshow('Red Ball Detection - Press q to quit', final_frame)
+    if cv.waitKey(0) & 0xFF == ord('q'):
+        break
+cap.release()
+output.release()
+cv.destroyAllWindows()
